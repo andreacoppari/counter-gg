5b79ba1ef992f13da845cacbc1fa44d7b9b50ed9
andreacoppari <andrea.coppari@proton.me>
2022-05-02 17:50:13 +0200

    feat(c.gg): added counter/tips/help/depressed commands

---
+++ get_champions.py
@@ -0,0 +1,27 @@
+from time import sleep
+import requests
+import json
+
+def get_champ_data():
+    with open("champions.json", "r", encoding="utf8") as champs:
+        data = json.load(champs)
+        champions = {}
+        for champ in data["data"]:
+            entry = {
+                "enemy_tips": champ['enemy_tips'],
+                "ally_tips": champ['ally_tips']
+                }
+            champions[champ["name"]] = entry
+
+    with open("champions_data.json", "w", encoding="utf8") as champion_dataset:
+        json.dump(champions, champion_dataset, indent=4)
+
+def get_champ_list():
+    with open("champions_data.json", "r", encoding="utf8") as ch:
+        data = json.load(ch)
+        champions = []
+        for champ in data:
+            champions.append(champ["name"])
+        return champions
+
+get_champ_data()
--- main.py
+++ main.py
@@ -3,9 +3,37 @@ import requests
 import json
 import os
 
+from get_champions import get_champ_list as champ_list
+
 client = discord.Client()
 
-champions = ['annie', 'ekko', 'yasuo']
+champions = champ_list()
+
+def get_enemy_tips(champ: str):
+    res = ""
+    with open("champions_data.json", "r", encoding="utf8") as ch:
+        data = json.load(ch)
+        if not data[champ]:
+            return "You spelled the champion wrong or this champ doesn't exist"
+        tips = data[champ]["enemy_tips"]
+        if len(tips) == 0:
+            return " - Sorry! no tips available yet!"
+        for tip in tips:
+            res += f" - {tip}\n"
+        return res
+
+def get_ally_tips(champ: str):
+    res = ""
+    with open("champions_data.json", "r", encoding="utf8") as ch:
+        data = json.load(ch)
+        if not data[champ]:
+            return "You spelled the champion wrong or this champ doesn't exist"
+        tips = data[champ]["ally_tips"]
+        if len(tips) == 0:
+            return " - Sorry! no tips available yet!"
+        for tip in tips:
+            res += f" - {tip}\n"
+        return res
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
@@ -24,12 +52,31 @@ async def on_message(msg: discord.Message):
     
     message = msg.content
 
-    if message.startswith("!!counter"):
-        champ = message.split()[1]
-        if champ.lower() in champions:
-            await msg.channel.send(f"{get_quote()}\n\nO meglio, impara a giocare -A.C.")
+    if message.startswith("!!help") or message.startswith("!!h"):
+        await msg.channel.send(
+            '''Here's the list of available commands:\n
+            !!help (or !!h) to display this message\n
+            !!counter (or !!c) for tips on how to counter a champ\n
+            !!tips (or !!t) for tips on how to play your champ\n
+            !!lost (or !!depressed) to get a random motivational quote
+        ''')
+
+    if message.startswith("!!counter") or message.startswith("!!c"):
+        champ = message.split()[1].lower().capitalize()
+        if champ in champions:
+            await msg.channel.send(f"Here's a list of tips to defeat {champ}:\n{get_enemy_tips(champ)}Good luck!")
         else:
-            await msg.channel.send("Scrivi $counter <campione> per info su come counterare un campione")
+            await msg.channel.send("Type !!counter <champ> or !!c <champ> to get tips on how to defeat a champ")
+
+    if message.startswith("!!tips") or message.startswith("!!t"):
+        champ = message.split()[1].lower().capitalize()
+        if champ in champions:
+            await msg.channel.send(f"Here's a list of tips for {champ}:\n{get_ally_tips(champ)}Good luck!")
+        else:
+            await msg.channel.send("Type !!tips <champ> or !!t <champ> to get tips on how to play a champ")
+
+    if message.startswith("!!lost") or message.startswith("!!depressed"):
+        await msg.channel.send(get_quote())
 
 token = os.environ.get("TOKEN")
 client.run(token)
